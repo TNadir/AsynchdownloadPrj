3e040548bc084ce4c9341b88a11caf134fa0f237
Nadir Tariverdiyev <nadir.tariverdiyev@gmail.com>
2018-11-05 17:27:30 +0300

    Created Task Parallel Library example for Task2

---
+++ Task2/AsyncDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class AsyncDemo
+    {
+        public string TestMethod(int callDuration, out int threadId)
+        {
+            Console.WriteLine("Test method begins.");
+            Thread.Sleep(callDuration);
+            threadId = Thread.CurrentThread.ManagedThreadId;
+            return String.Format("My call time was {0}.", callDuration.ToString());
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Task2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //Task Parallel Library
+            RunParallelAsync();
+            Console.ReadLine();
+        }
+
+        private static void DownloadWebsite(string url)
+        {
+            WebClient client = new WebClient();
+            Console.WriteLine(url+"-"+client.DownloadString(url).Length); 
+        }
+
+        public static async void RunParallelAsync()
+        {
+            List<string> websites = new List<string>();
+            websites.Add("https://www.yahoo.com");
+            websites.Add("https://www.google.com");
+            websites.Add("https://www.microsoft.com");
+            websites.Add("https://www.cnn.com");
+            websites.Add("https://www.amazon.com");
+            websites.Add("https://www.facebook.com");
+            websites.Add("https://www.twitter.com");
+            websites.Add("https://www.codeproject.com");
+            websites.Add("https://www.stackoverflow.com");
+            websites.Add("https://en.wikipedia.org/wiki/.NET_Framework");
+            await Task.Run(() =>
+            {
+                Parallel.ForEach(websites, DownloadWebsite);
+            });
         }
     }
 }
